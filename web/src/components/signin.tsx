709e36a6f5cc9d5ce08a3af43d0ad840dcb54d81
KZTN <kakaiocesar2@gmail.com>
2020-07-04 13:40:23 -0300

    :recycle: Refactored signin & signup components
    all changes are functional

--- web/src/components/signin.tsx
+++ web/src/components/signin.tsx
@@ -1,25 +1,62 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-
-import './styles.css'
+import React, { useState, FormEvent, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../services/api";
+import "./styles.css";
 
 const SignIn = () => {
-  const history = useHistory()
+  const history = useHistory();
+  const [emailfield, setEmailfield] = useState<string>("");
+  const [passwordfield, setSasswordfield] = useState<string>("");
 
-  function goToHome() {
-    history.push('/home')
+  useEffect(() => {
+    if (localStorage.getItem("id")) {
+      history.push("/home");
+    }
+  }, [history]);
+
+  async function getData() {
+    await api
+      .post("/sessions", { email: emailfield, password: passwordfield })
+      .then((response) => {
+        localStorage.setItem("id", response.data);
+        history.push("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    getData();
   }
 
   return (
     <div className="form">
-      <input type="text" name="email" placeholder="E-mail" />
-      <input type="password" name="password" placeholder="Senha" />
-
-      <div className="footer">
-        <button onClick={goToHome}>Entrar</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="E-mail"
+          required
+          value={emailfield}
+          onChange={(e) => setEmailfield(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Senha"
+          required
+          minLength={6}
+          value={passwordfield}
+          onChange={(e) => setSasswordfield(e.target.value)}
+        />
+        <div className="footer">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- web/src/components/signup.tsx
+++ web/src/components/signup.tsx
@@ -1,31 +1,95 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
-
-import './styles.css'
+import React, { useState, FormEvent, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../services/api";
+import Input from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import "./styles.css";
 
 const SignUp = () => {
-  const history = useHistory()
-
-  function goToHome() {
-    history.push('/home')
+  const history = useHistory();
+  const [namefield, setNamefield] = useState<string>("");
+  const [phonefield, setPhonefield] = useState<string>("");
+  const [cityfield, setCityfield] = useState<string>("");
+  const [emailfield, setEmailfield] = useState<string>("");
+  const [passwordfield, setSasswordfield] = useState<string>("");
+  useEffect(() => {
+    if (localStorage.getItem("id")) {
+      history.push("/home");
+    }
+  }, [history]);
+  async function handleData() {
+    await api
+      .post("/users", {
+        email: emailfield,
+        password: passwordfield,
+        city: cityfield,
+        phone: phonefield,
+      })
+      .then((response) => {
+        localStorage.setItem("id", response.data.id);
+        history.push("/home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    handleData();
+  }
   return (
     <div className="content">
       <div className="form">
-        <input type="text" name="name" placeholder="Nome" />
-        <input type="text" name="address" placeholder="Endereço" />
-        <input type="text" name="complement" placeholder="Complemento" />
-        <input type="text" name="cep" placeholder="CEP" />
-        <input type="text" name="email" placeholder="E-mail" />
-        <input type="password" name="password" placeholder="Senha" />
-
-        <div className="footer">
-          <button onClick={goToHome}>Cadastrar</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            required
+            value={namefield}
+            onChange={(e) => setNamefield(e.target.value)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="E-mail"
+            required
+            value={emailfield}
+            onChange={(e) => setEmailfield(e.target.value)}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="Cidade"
+            required
+            value={cityfield}
+            onChange={(e) => setCityfield(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Senha"
+            required
+            minLength={6}
+            value={passwordfield}
+            onChange={(e) => setSasswordfield(e.target.value)}
+          />
+          <Input
+            defaultCountry="BR"
+            name="phone"
+            id="phone"
+            value={phonefield}
+            onChange={setPhonefield}
+            placeholder="Telefone"
+            style={{ width: "290px" }}
+          />
+          <div className="footer">
+            <button type="submit">Cadastrar</button>
+          </div>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
