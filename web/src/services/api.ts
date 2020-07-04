38085855cadcccaef28d2d9f5be41bc949f6c5f8
KZTN <kakaiocesar2@gmail.com>
2020-07-04 12:20:24 -0300

    :recycle: Refactored API baseURL
    all changes are functional

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: "https://megahack2020-server.herokuapp.com",
 });
 
 export default api;
