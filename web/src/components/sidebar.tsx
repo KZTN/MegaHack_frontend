842de69091789ca0b14a39a329e811553154fcfa
KZTN <kakaiocesar2@gmail.com>
2020-07-04 13:45:43 -0300

    :sparkles: Added logout options on sidebar component
    all changes are functional

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -1,40 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { FaHome, FaRegStar, FaShoppingCart, FaRegFileAlt } from 'react-icons/fa'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaRegStar,
+  FaShoppingCart,
+  FaRegFileAlt,
+  FaDoorOpen,
+} from "react-icons/fa";
 
 const SideBar = () => {
-
+  function handleLogout() {
+    localStorage.removeItem("id");
+  }
   return (
     <>
       <div className="menu-wrapper">
         <div className="links-wrapper">
           <Link to="/profile">
-            <img src={require("../assets/user-profile.jpg")} alt="olar" className="img-menu" />
+            <img
+              src={require("../assets/user-profile.jpg")}
+              alt="olar"
+              className="img-menu"
+            />
             <div className="profile-link">
               <strong>[Nome]</strong>
               <small>Ver Perfil</small>
             </div>
           </Link>
           <Link to="/home">
-            <FaHome size={20}/>
+            <FaHome size={20} />
             <label>Navegar</label>
           </Link>
           <Link to="/favorite">
-            <FaRegStar size={20}/>
+            <FaRegStar size={20} />
             <label>Favoritos</label>
           </Link>
           <Link to="/">
-            <FaShoppingCart size={20}/>
+            <FaShoppingCart size={20} />
             <label>Pedidos</label>
           </Link>
           <Link to="/">
-            <FaRegFileAlt size={20}/>
+            <FaRegFileAlt size={20} />
             <label>Histórico</label>
+          </Link>
+          <Link to="/" onClick={handleLogout}>
+            <FaDoorOpen size={20} />
+            <label>Sair</label>
           </Link>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default SideBar;
